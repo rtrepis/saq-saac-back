9e65ec4676f9960c0b8535bd73e651b606ea48f8
Ramon Trepat <r.trepis@gmail.com>
2022-08-30 15:18:49 +0200

    Set hashCreator call in try

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,18 +1,9 @@
 import "../loadEnvironment";
-import jsonwebtoken from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { Jwt } from "../database/types/interfaces";
-
-export const createToken = (payload: Jwt) =>
-  jsonwebtoken.sign(payload, process.env.SECRET);
-
-export const verifyToken = (token: string) =>
-  jsonwebtoken.verify(token, process.env.SECRET);
 
 export const hashCreator = (text: string) => {
   const salt = 10;
   return bcrypt.hash(text, salt);
 };
 
-export const hashCompare = (text: string, hash: string) =>
-  bcrypt.compare(text, hash);
+export default hashCreator;

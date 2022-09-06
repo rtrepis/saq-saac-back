ee189fe8008781dbe2ce83f9c741bffde8f7ddfd
Ramon Trepat <r.trepis@gmail.com>
2022-09-06 11:57:52 +0200

    Added controller sequences function getAllSequences

---
+++ src/server/controllers/sequence/sequencesController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import Sequence from "../../../database/models/Sequence";
+
+const getAllSequence = async (req: Request, res: Response) => {
+  const sequences = await Sequence.find();
+
+  res.status(200).json({ sequences });
+};
+
+export default getAllSequence;

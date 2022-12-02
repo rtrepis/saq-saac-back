c3b774eaf9d7a30cac18017a03d6a9ddffc890b6
Ramon Trepat <r.trepis@gmail.com>
2022-12-02 16:13:45 +0100

    Removed no use code

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,6 @@
 import "../loadEnvironment";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import jwtDecode from "jwt-decode";
 import { UserJwtPayload } from "../database/types/UsersInterfaces";
 
 export const hashCreator = (text: string) => {
@@ -17,14 +16,3 @@ export const createToken = (payload: UserJwtPayload | string) =>
 
 export const verifyToken = (token: string) =>
   jwt.verify(token, process.env.SECRET);
-
-export const decodeToken = (token: string) => {
-  const payloadTokenForgot: {
-    email: string;
-    iat: number;
-  } = jwtDecode(token);
-
-  return {
-    email: payloadTokenForgot.email,
-  };
-};
